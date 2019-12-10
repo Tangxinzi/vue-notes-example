a727c5fd2ad0929d529a7e8d4f1abc161bfe18d5
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-10 15:45:15 +0800

    显示笔记列表

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const Note = {
+  props: [
+    'entityObject'
+  ],
+  data() {
+    return {
+      entity: this.entityObject
+    }
+  },
+  template:  `
+    <div class="item">
+      <div class="content">
+        <div class="header">
+          {{ entity.body }}
+        </div>
+      </div>
+    </div>
+  `
+}
+
+const Notes = {
   data() {
     return {
       entities: []
@@ -17,16 +37,6 @@ const Note = {
         console.log(_entities)
       })
   },
-  template:  `
-    <div class="item">
-      <div class="content">
-        <div class="header">笔记</div>
-      </div>
-    </div>
-  `
-}
-
-const Notes = {
   components: {
     'note': Note
   },
@@ -38,9 +48,7 @@ const Notes = {
       </h4>
       <a class="ui right floated basic violet button">添加笔记</a>
       <div class="ui divided items">
-        <note></note>
-        <note></note>
-        <note></note>
+        <note v-for="entity in entities" v-bind:entityObject="entity" v-bind:key="entity.$loki"></note>
       </div>
     </div>
   `
